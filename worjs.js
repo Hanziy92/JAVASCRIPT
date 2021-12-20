fe06288185d9cfc791666ba26f8e2d9c8202ac6e
Sofia <hanziypetro@gmail.com>
2021-12-20 17:46:50 +0200

    DoWhileFor

--- worjs.js
+++ worjs.js
@@ -1,29 +1,58 @@
 "use strict"; 
 
-const num = 52;
+// Умови
+//const num = 52;
 
-if (num<50){
-   console.log('Не віно');
-} else if (num>100){
-   console.log('Не віно');
-} else {
-   console.log('Віно');
-};
+// if (num<50) {
+//    console.log('Не віно');
+// } else if (num>100) {
+//    console.log('Не віно');
+// } else {
+//    console.log('Віно');
+// };
+
+// (num === 50) ? console.log('Віно') : console.log('Не віно');
+
+// switch (num) {
+//    case 50:
+//       console.log('Не віно');
+//       break;
+//    case 34:
+//       console.log('Не віно');
+//       break;
+//    case 100:
+//       console.log('Не віно');
+//       break;
+//    default:
+//       console.log('Не в цей раз');
+// }
 
 
-(num === 50) ? console.log('Віно') : console.log('Не віно');
+// Цикли
+
+let num = 50;
+
+// while (num<55) {
+//    console.log(num);
+//    num++;
+// }
 
 
-switch (num){
-   case 50:
-      console.log('Не віно');
+// do {
+//    console.log(num);
+//    num++;
+// }
+// while (num<55);
+
+// for (let i =1; i<8; i++) {
+//    console.log(num);
+//    num++;
+// }
+
+for (let i =1; i<8; i++) {
+   if (i === 6) {
       break;
-   case 34:
-      console.log('Не віно');
-      break;
-   case 100:
-      console.log('Не віно');
-      break;
-   default:
-      console.log('Не в цей раз');
+   }
+   console.log(num);
+   num++;
 }
