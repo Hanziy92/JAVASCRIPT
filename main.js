d9367a6b12d94618f070c4a2dc34f1a44e97b70f
Sofia <hanziypetro@gmail.com>
2021-12-21 15:33:02 +0200

    functionPractice

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 "use strict"; 
 
-const numberFilms = +prompt('скільки фільмів ви подивились?', '');
+let numberFilms;
+
+function start() {
+   numberFilms = +prompt('скільки фільмів ви подивились?', '');
+   while(numberFilms == '' || numberFilms == null || isNaN(numberFilms)) {
+      numberFilms = +prompt('скільки фільмів ви подивились?', '');
+   }
+}
+
+start();
 
 const personalMouvisDB = {
    count: numberFilms,
@@ -10,26 +19,50 @@ const personalMouvisDB = {
    privat: false
 };
 
+function rememberMyFilms (){
+   for (let i = 0; i<2; i++) {  
 
-for (let i = 0; i<2; i++) {  
-   const a = prompt('Один із останіх пееглянутих фільмів?', ''),
-         b = prompt('На скільки його оцінете?', '');
-   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-      personalMouvisDB.movies[a] = b;
-   } else {
-      i--;
+      const a = prompt('Один із останіх пееглянутих фільмів?', ''),
+            b = prompt('На скільки його оцінете?', '');
+
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+         personalMouvisDB.movies[a] = b;
+      } else {
+         i--;
+      }
    }
 }
 
-if (personalMouvisDB.count < 10) {
-   console.log('Подивились доволі мало фільмів');
-} else if (personalMouvisDB.count >= 10 && personalMouvisDB.count < 30) {
-   console.log('Ви класичний глядач');
-} else if (personalMouvisDB.count >= 30) {
-   console.log('Ви кіноман');
-} else {
-   console.log('Відбулася помилка');
-} 
+rememberMyFilms ();
 
 
-console.log(personalMouvisDB);
+
+function detectPersonalLevel(){
+   if (personalMouvisDB.count < 10) {
+      console.log('Подивились доволі мало фільмів');
+   } else if (personalMouvisDB.count >= 10 && personalMouvisDB.count < 30) {
+      console.log('Ви класичний глядач');
+   } else if (personalMouvisDB.count >= 30) {
+      console.log('Ви кіноман');
+   } else {
+      console.log('Відбулася помилка');
+   } 
+}
+
+detectPersonalLevel();
+
+
+function showMyDB(hidden) {
+   if (!hidden) {  
+      console.log(personalMouvisDB);
+   } 
+}
+
+showMyDB(personalMouvisDB.privat);
+
+function writeYourGeners(){
+   for (let i = 1; i<=3; i++ ) { 
+      personalMouvisDB.genders[i-1] = prompt(`Ваш любимий жанр під номером ${i}`);
+   }
+}
+writeYourGeners();
--- worjs.js
+++ worjs.js
@@ -1,148 +1,149 @@
 "use strict"; 
 
 // Умови
-const num = 52;
+// const num = 52;
 
-if (num<50) {
-   console.log('Не віно');
-} else if (num>100) {
-   console.log('Не віно');
-} else {
-   console.log('Віно');
-};
+// if (num<50) {
+//    console.log('Не віно');
+// } else if (num>100) {
+//    console.log('Не віно');
+// } else {
+//    console.log('Віно');
+// };
 
-(num === 50) ? console.log('Віно') : console.log('Не віно');
+// (num === 50) ? console.log('Віно') : console.log('Не віно');
 
-switch (num) {
-   case 50:
-      console.log('Не віно');
-      break;
-   case 34:
-      console.log('Не віно');
-      break;
-   case 100:
-      console.log('Не віно');
-      break;
-   default:
-      console.log('Не в цей раз');
-}
+// switch (num) {
+//    case 50:
+//       console.log('Не віно');
+//       break;
+//    case 34:
+//       console.log('Не віно');
+//       break;
+//    case 100:
+//       console.log('Не віно');
+//       break;
+//    default:
+//       console.log('Не в цей раз');
+// }
 
 
 // Цикли
 
-let num = 50;
+// let num = 50;
 
-while (num<55) {
-   console.log(num);
-   num++;
-}
-
-
-do {
-   console.log(num);
-   num++;
-}
-while (num<55);
-
-for (let i =1; i<8; i++) {
-   console.log(num);
-   num++;
-}
-
-for (let i =1; i<8; i++) {
-   if (i === 6) {
-      break;
-   }
-   console.log(i);
-   i++;
-}
+// while (num<55) {
+//    console.log(num);
+//    num++;
+// }
 
 
-for (let i =1; i<8; i++) {
-   if (i === 6) {
-      continue;
-   }
-   console.log(i);
-}
+// do {
+//    console.log(num);
+//    num++;
+// }
+// while (num<55);
+
+// for (let i =1; i<8; i++) {
+//    console.log(num);
+//    num++;
+// }
+
+// for (let i =1; i<8; i++) {
+//    if (i === 6) {
+//       break;
+//    }
+//    console.log(i);
+//    i++;
+// }
+
+
+// for (let i =1; i<8; i++) {
+//    if (i === 6) {
+//       continue;
+//    }
+//    console.log(i);
+// }
 
 
 
 
 //Функції
 
-let num = 20;
+// let num = 20;
 
-function showFirstMessage(text) {
-   console.log(text);
-   let num =10;
-   console.log(num);
-}
+// function showFirstMessage(text) {
+//    console.log(text);
+//    let num =10;
+//    console.log(num);
+// }
 
-showFirstMessage("HEllo World");
-console.log(num);
-
-function calc(a, b) {
-   return (a+b);
-}
-console.log( calc(4, 5) );
-console.log( calc(9, 5) );
-console.log( calc(15, 57) );
+// showFirstMessage("HEllo World");
 
 
-function ret() {
-   let num = 50;
-   return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-const good = function(){
-   console.log("Hello")
-};
+// function calc(a, b) {
+//    return (a+b);
+// }
+// console.log( calc(4, 5) );
+// console.log( calc(9, 5) );
+// console.log( calc(15, 57) );
 
 
-const arrow = (a, b) => {return a+b}
+// function ret() {
+//    let num = 50;
+//    return num;
+// }
 
-console.log( arrow(5, 6))
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const good = function(){
+//    console.log("Hello")
+// };
 
 
+// const arrow = (a, b) => {return a+b}
 
-
-// методи свойства чисел і рядків
-
-const str = 'test';
-
-const arr = [1, 2, 4]; 
-
-console.log(str.length);
-console.log(arr.length);
-console.log(str.toUpperCase()); // Врхній регістр 
-console.log(str.toLowerCase()); // Нижній регістр
+// console.log( arrow(5, 6))
 
 
 
-const frut = "Some fruits";
-console.log(frut.indexOf('fruits'));
-console.log(frut.indexOf('q')); // -1 значить немає такого значення в рядку
+
+// // методи свойства чисел і рядків
+
+// const str = 'test';
+
+// const arr = [1, 2, 4]; 
+
+// console.log(str.length);
+// console.log(arr.length);
+// console.log(str.toUpperCase()); // Врхній регістр 
+// console.log(str.toLowerCase()); // Нижній регістр
 
 
-const logg = "Hello World"; 
-console.log(logg.slice(6)); // від аргумента і до кінця
-console.log(logg.slice(6, 11)); // до якого елемента не включаючи його 
-console.log(logg.slice(6, 10));
 
-console.log(logg.substr(6, 5)); // end arg скільки символів треба вирізати
+// const frut = "Some fruits";
+// console.log(frut.indexOf('fruits'));
+// console.log(frut.indexOf('q')); // -1 значить немає такого значення в рядку
 
 
-// Numbers
+// const logg = "Hello World"; 
+// console.log(logg.slice(6)); // від аргумента і до кінця
+// console.log(logg.slice(6, 11)); // до якого елемента не включаючи його 
+// console.log(logg.slice(6, 10));
+
+// console.log(logg.substr(6, 5)); // end arg скільки символів треба вирізати
 
 
-const numer = 12.2;
+// // Numbers
 
-console.log(Math.round(numer));
 
-const test = '12.2px';
+// const numer = 12.2;
 
-console.log(parseInt(test));  // Зі строки вертає в число заукруглює до цілого 12
-console.log(parseFloat(test));  // вертає дробні числа  12.2
+// console.log(Math.round(numer));
+
+// const test = '12.2px';
+
+// console.log(parseInt(test));  // Зі строки вертає в число заукруглює до цілого 12
+// console.log(parseFloat(test));  // вертає дробні числа  12.2
+
