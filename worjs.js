2c936d960588854e786b15fccb7b5fff0dce8058
Sofia <hanziypetro@gmail.com>
2021-12-21 12:26:43 +0200

    Function

--- worjs.js
+++ worjs.js
@@ -64,3 +64,37 @@
 //    }
 //    console.log(i);
 // }
+
+
+//Функції
+
+let num = 20;
+
+function showFirstMessage(text) {
+   console.log(text);
+   let num =10;
+   console.log(num);
+}
+
+showFirstMessage("HEllo World");
+console.log(num);
+
+function calc(a, b) {
+   return (a+b);
+}
+console.log( calc(4, 5) );
+console.log( calc(9, 5) );
+console.log( calc(15, 57) );
+
+
+function ret() {
+   let num = 50;
+   return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const good = function(){
+   console.log("Hello")
+};
